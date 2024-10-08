35eca81f8a57215a62ca7d37f5c5b7a0515a6cf3
Peyton11 <peyton.gardner03@gmail.com>
2024-10-08 17:27:56 -0400

    Add Parser class

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,11 +1,4 @@
 // Parser.hpp
 // The parser uses the tokens produced by the lexer to build an abstract syntax tree (AST). This is a structured representation of the document.
 
-#ifndef PARSER_HPP
-#define PARSER_HPP
-
-class Parser {
-
-};
-
-#endif
+#include "Parser.hpp"
