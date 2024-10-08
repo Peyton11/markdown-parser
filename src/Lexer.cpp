83a4cd82d0993801ac67ee35ea43d7b2eda66048
Peyton11 <peyton.gardner03@gmail.com>
2024-10-08 17:48:33 -0400

    Add methods for detecting Markdown headings

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -11,3 +11,33 @@ std::vector<Token> Lexer::lex(std::string& line) {
     std::vector<Token> temp;
     return temp;
 }
+
+// Checks if the line is a level 1 heading in Mardown
+bool Lexer::isHeading1(std::string& line) {
+    return (line[0] == '#' && line[1] == ' ');
+}
+
+// Checks if the line is a level 2 heading in Mardown
+bool Lexer::isHeading2(std::string& line) {
+    return (line[0] == '#' && line[1] == '#' && line[2] == ' ');
+}
+
+// Checks if the line is a level 3 heading in Mardown
+bool Lexer::isHeading3(std::string& line) {
+    return (line[0] == '#' && line[1] == '#' && line[2] == '#' && line[3] == ' ');
+}
+
+// Checks if the line is a level 4 heading in Mardown
+bool Lexer::isHeading4(std::string& line) {
+    return (line[0] == '#' && line[1] == '#' && line[2] == '#' && line[3] == '#' && line[4] == ' ');
+}
+
+// Checks if the line is a level 5 heading in Mardown
+bool Lexer::isHeading5(std::string& line) {
+    return (line[0] == '#' && line[1] == '#' && line[2] == '#' && line[3] == '#' && line[4] == '#' && line[5] == ' ');
+}
+
+// Checks if the line is a level 6 heading in Mardown
+bool Lexer::isHeading6(std::string& line) {
+    return (line[0] == '#' && line [1] == '#' && line[2] == '#' && line[3] == '#' && line[4] == '#' && line[5] == '#' && line[6] == ' ');
+}
--- src/Lexer.hpp
+++ src/Lexer.hpp
@@ -15,6 +15,24 @@ public:
 
 private:
 
+    // Checks if the line is a level 1 heading in Mardown
+    bool isHeading1(std::string& line);
+
+    // Checks if the line is a level 2 heading in Mardown
+    bool isHeading2(std::string& line);
+
+    // Checks if the line is a level 3 heading in Mardown
+    bool isHeading3(std::string& line);
+
+    // Checks if the line is a level 4 heading in Mardown
+    bool isHeading4(std::string& line);
+
+    // Checks if the line is a level 5 heading in Mardown
+    bool isHeading5(std::string& line);
+
+    // Checks if the line is a level 6 heading in Mardown
+    bool isHeading6(std::string& line);
+
     std::vector<Token> tokens;
 };
 
