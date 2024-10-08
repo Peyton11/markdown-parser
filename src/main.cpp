a6d2e214b98fbd71ad371e25bbbb7d5226227208
Peyton11 <peyton.gardner03@gmail.com>
2024-10-08 16:26:14 -0400

    Read file contents into lines

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 // main.cpp
 // Driver file for compiling markdown into a structured language.
 
+#include <iostream>
 #include <fstream>
 
 int main(int argc, char* argv[]) {
+
+    std::ifstream file("../examples/example.md");
+    if (!file) {
+        std::cerr << "Unable to open file\n";
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        // @TODO: lex each line
+    }
+
+    // @TODO: parse tokens from lexer
+
+    file.close();
 
     return 0;
 }
